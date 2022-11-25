038a15ac2bfe67b93bd53c6d338df7d4dece3f83
Mohamed Ali <76604718+mohamedali12317@users.noreply.github.com>
2022-11-25 14:43:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('.btn');
+
+let div = document.querySelector('.hide');
+
+
+btn.style.color= 'red';
+
+btn.onclick = function (){
+ console.log('wo');
+}
+
+btn.onclick = function (){
+ div.style.display='block'; 
+}
+
+
+
